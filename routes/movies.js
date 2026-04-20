baae18f266f09913017081e9351cfa19c63f04c5
salv-ritrovato <salv.ritrovato@gmail.com>
2026-04-20 12:18:28 +0200

    show route working correctly, public and img folder created

--- routes/movies.js
+++ routes/movies.js
@@ -3,9 +3,18 @@ import connection from "../database/connection.js";
 
 const router = express.Router();
 
+// INDEX
 router.get("/", async (req, res) => {
     const [movies] = await connection.query("SELECT * FROM movies");
     res.json(movies);
+});
+
+// SHOW
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const [movies] = await connection.query("SELECT * FROM movies WHERE id = ?", [id]);
+    const movie = movies[0];
+    res.json(movie);
 });
 
 export default router;
