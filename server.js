80a0368e6f2febfb326f36db7e1f92ed22ace0b2
salv-ritrovato <salv.ritrovato@gmail.com>
2026-04-20 12:01:30 +0200

    app linked to mysql DB correctly

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connection from "./database/connection.js"
 
 dotenv.config();
 
@@ -9,9 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server running");
+    res.send("Server running");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
