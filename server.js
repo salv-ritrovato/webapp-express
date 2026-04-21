1e7885d14a32d7efb881f02d96f3fc6083addeea
salv-ritrovato <salv.ritrovato@gmail.com>
2026-04-21 13:09:02 +0200

    fixed img path

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
 }))
 
 app.use(express.json());
-app.use("/images", express.static("public/img"));
+app.use("/images", express.static("public/img/movies_cover"));
 
 app.get("/", (req, res) => {
     res.send("Server running");
